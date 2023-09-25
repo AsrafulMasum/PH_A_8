7b5e4db25b77b00b317d5502490a4dc8d250e08d
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-09-25 15:08:36 +0600

    banner section created with background image

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import './banner.css'
+const Banner = () => {
+  return (
+    <div>
+      <section className="h-[70vh] flex flex-col justify-center text-center -mt-24">
+        <h2 className="text-[#0B0B0B] font-bold text-4xl pb-10 opacity-95">I Grow By Helping People In Need</h2>
+        <div>
+          <input className="w-1/3 h-12 border-[1px] p-4 rounded-l-lg" type="text" placeholder="Search here...."/>
+          <button className="h-12 w-24 bg-[#FF444A] rounded-r-lg text-white">Search</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;
