6c088d05af11fa3908c529fafb06124dd5166233
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-09-26 06:24:57 +0600

    donate button functionality applied to save into localstorage and toast showed

--- src/pages/DonationDetails.jsx
+++ src/pages/DonationDetails.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import { getLSIds, handleSaveToLS } from "../utility/localStorage";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const DonationDetails = () => {
   const data = useLoaderData()
@@ -12,6 +15,19 @@ const DonationDetails = () => {
       setCurrentData(foundData)
     }
   }, [data, idInt])
+
+  const handleDonate = () => {
+    const donatedIds = getLSIds()
+    const isExists = donatedIds.find(donatedId => donatedId === idInt)
+    if(!isExists){
+      toast.success('You have donated successfully.')
+    }
+    else{
+      toast.error('You have donated to this fund already.')
+    }
+    handleSaveToLS(idInt)
+  }
+
   return (
     <div className="my-10">
       <div>
@@ -21,7 +37,7 @@ const DonationDetails = () => {
         background: '#0B0B0B7F',
         backgroundBlendMode: 'overlay'
       }} className="py-8 pl-8 relative -top-28 rounded-b-lg">
-        <button  style={{
+        <button onClick={handleDonate} style={{
           backgroundColor: `${currentData.text_button_bg_color}`
         }} className="px-6 py-3 rounded-md text-white font-semibold">Donate ${currentData.price}</button>
       </div>
@@ -31,6 +47,7 @@ const DonationDetails = () => {
       <div>
         <p className="text-lg text-[#0B0B0BB3]">{currentData.description}</p>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
---
+++ src/utility/localStorage.js
@@ -0,0 +1,18 @@
+const getLSIds = () => {
+  const storedLSData = localStorage.getItem('donationId');
+  if(storedLSData){
+    return JSON.parse(storedLSData)
+  }
+  return []
+}
+
+const handleSaveToLS = (id) => {
+  const storedLSData = getLSIds()
+  const isExists = storedLSData.find(donationId => donationId === id)
+  if(!isExists){
+    storedLSData.push(id)
+    localStorage.setItem('donationId', JSON.stringify(storedLSData))
+  }
+}
+
+export {handleSaveToLS, getLSIds}
